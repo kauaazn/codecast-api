3b2a7776edbfe0bbffae758cbfa03533b0a6570c
Kauã <kauaduda2014@gmail.com>
2026-06-20 21:26:46 -0300

    chore: project setup with spring boot, h2 and jpa

---
+++ codecast/src/main/java/com/code/codecast/CodecastApplication.java
@@ -0,0 +1,13 @@
+package com.code.codecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecastApplication.class, args);
+	}
+
+}
---
+++ codecast/src/test/java/com/code/codecast/CodecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.codecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
